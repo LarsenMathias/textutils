22f11bb50fd7e41a8c899ddf9b0d4eea7fbeff9d
LarsenMathias <88501525+LarsenMathias@users.noreply.github.com>
2021-08-31 23:27:36 +0530

    Update settings.py

--- textutils/settings.py
+++ textutils/settings.py
@@ -124,7 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles')) 
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
 
