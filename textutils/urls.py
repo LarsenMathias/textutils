9eef4d4ccfd87d6acd255d99ab861c372786e215
Dark Lord <larsenm2020@gmail.com>
2021-08-31 21:02:10 +0530

    Initial commit

---
+++ textutils/urls.py
@@ -0,0 +1,45 @@
+"""textutils URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('analyze',views.analyze,name='analyze'),
+    path('LeeshaLarsenGarments', views.LeeshaLarsenGarments, name='LeeshaLarsenGarments'),
+    # path('removepunc',views.removepunc,name='rempun'),
+    # path('Newlineremover',views.Newlineremover,name='Newlineremover'),
+    path('home',views.home,name='home'),
+    # path('ex1', views.ex1, name='ex1'),
+    path('GABRIALFURNITURE', views.GABRIALFURNITURE, name="GABRIALFURNITURE"),
+    path('ContactAldron',views.ContactAldron, name="ContactAldron"),
+    path('DigitalPrinting',views.DigitalPrinting,name="DigitalPrinting"),
+    path('ContactRajath',views.ContactRajath,name="ContactRajath"),
+    path("",views.GABRIALFURNITURE,name="zir.jpeg"),
+    path("",views.index,name="julianacomplex.jpeg"),
+    path("",views.DigitalPrinting,name="printer.jpeg"),
+    path("",views.LeeshaLarsenGarments,name="dress.jpeg"),
+    path("",views.LeeshaLarsenGarments,name="dress back.jpeg"),
+    path("",views.LeeshaLarsenGarments,name="dress price.jpeg"),
+    path("",views.LeeshaLarsenGarments,name="dresstrial.jpeg"),
+    path("contactus",views.contactus,name="contactus.html"),
+
+]
+urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ textutils/views.py
@@ -0,0 +1,58 @@
+#I have created this file -Larsen
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+    params={'name':'Lawrence','place':'Kolalgiri Laximinagar'}
+    return render(request,'index.html',params)
+#
+# def Home(request):
+#     return render(request,'home.html')
+#     return HttpResponse("Home")
+def analyze(request):
+    djtext=(request.GET.get('analyze','default'))
+    print(analyze)
+    print(djtext)
+    return HttpResponse("Your form is submitted ")
+def GABRIALFURNITURE(request):
+    GABRIALFURNITURE={'dark':'Larsen'}
+    return render(request,'GABRIALFURNITURE.html',GABRIALFURNITURE)
+def LeeshaLarsenGarments(request):
+    leesha={'name':'Lawrence','place':'kolalgiri'}
+    return render(request,'LeeshaLarsenGarments.html',leesha)
+def home(request):
+    home = {'dark': 'Larsen'}
+    return render(request,'home.html',home)
+
+
+# def capfirst(request):
+#     return HttpResponse("Capitalize first ")
+#def home(request):
+    #return HttpResponse("<h1> This complex has cloths,Furnitures,Printer to print banners machine<h2> Press back to go back to main Menue <a href='/'> back</a>")
+ #def capfirst(request):
+#     return HttpResponse("Capitalize first ")
+# def Newlineremover(request):
+#     return HttpResponse("Newlineremover ")
+
+# def home(request):
+#     sites = ['''<h1>For cloths </h1><a href = "" >youtube video</a>''',
+#              '''<h1>For Interaction </h1><a href = "https://www.facebook.com" >Facebook</a>''',
+#              '''<h1>For Insight   </h1><a href = "https://www.ted.com/talks" >Ted Talk</a>''',
+#              '''<h1>For Internship   </h1><a href="https://internshala.com" >Intenship</a>''',
+#              ]
+#     return HttpResponse((sites))
+def ContactAldron(request):
+    ald = {'name': 'Aldron', 'shop': 'GabrialFurniture'}
+    return render(request,'ContactAldron.html')
+def DigitalPrinting(request):
+    return render(request,'DigitalPrinting.html')
+def ContactRajath(request):
+    ald = {'name': 'Rajath', 'shop': 'DigitalPrinting'}
+    return render(request,'ContactRajath.html')
+# def zir(request):
+#     return render(request,'zir.jpeg')
+def contactus(request):
+    ald = {'name': 'Rajath', 'shop': 'DigitalPrinting'}
+    return render(request,'contactus.html')
+
+
+
